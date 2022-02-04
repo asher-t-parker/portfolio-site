eb0ab3da4ed75d5bbcc8e9ae89ebf4ff83928f4c
Asher T. Parker <asher.parker@outlook.com>
2022-02-03 21:20:59 -0600

    Add animation

--- resources/JS/animation.js
+++ resources/JS/animation.js
@@ -1,1 +1,57 @@
-import anime from 'https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js';
+let animation = anime.timeline({
+    loop: true
+    });
+
+// ADJ 1
+animation.add({
+    targets: '.adj1 span',
+    translateY: 20,
+    opacity: 1,
+    delay: anime.stagger(50),
+})
+animation.add({
+    targets: 'adj1 span',
+    duration: 800
+})
+animation.add({
+    targets: '.adj1 span',
+    translateY: -20,
+    opacity: 0,
+    delay: anime.stagger(80),
+})
+
+//ADJ 2
+animation.add({
+    targets: '.adj2 span',
+    translateY: 20,
+    opacity: 1,
+    delay: anime.stagger(50),
+})
+animation.add({
+    targets: 'adj2 span',
+    duration: 800
+})
+animation.add({
+    targets: '.adj2 span',
+    translateY: -20,
+    opacity: 0,
+    delay: anime.stagger(80),
+})
+
+//ADJ 3
+animation.add({
+    targets: '.adj3 span',
+    translateY: 20,
+    opacity: 1,
+    delay: anime.stagger(50),
+})
+animation.add({
+    targets: 'adj3 span',
+    duration: 800
+})
+animation.add({
+    targets: '.adj3 span',
+    translateY: -20,
+    opacity: 0,
+    delay: anime.stagger(80),
+})
